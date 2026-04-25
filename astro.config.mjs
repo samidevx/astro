0522832d2a4cd9cbc20496cf37dcac682b46d3ed
samidevx <islem.bouagada@gmail.com>
2026-04-25 06:04:33 +0200

    Remove platformProxy to fix Cloudflare build crash

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,5 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  })
+  adapter: cloudflare()
 });
